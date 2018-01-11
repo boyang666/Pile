5e429dcdf2837a12fef86d41d7be96f47d6cad2b
boyang666 <boyang.wang1993@gmail.com>
2018-01-11 19:26:12 +0100

    version 1.0 import the initial code

---
+++ src/main/java/com/polytechtours/di5/Pile/App.java
@@ -0,0 +1,13 @@
+package com.polytechtours.di5.Pile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
